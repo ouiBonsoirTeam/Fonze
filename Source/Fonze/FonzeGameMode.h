7428e573d8d717e69d3c6fd7a78e02cefb43877e
Jordinaire <jordi.bastide@gmail.com>
2016-01-09 09:56:51 +0100

    Initial commit

---
+++ Source/Fonze/FonzeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "FonzeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FONZE_API AFonzeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
