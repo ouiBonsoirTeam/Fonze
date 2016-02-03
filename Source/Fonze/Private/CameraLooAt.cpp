7eb0887ef04af8967203ac2ca8fc97bf9238e6a5
kingofthebongo <carpentier.thibaud@gmail.com>
2016-02-03 12:09:47 +0100

    CameraLookAt but bug

--- Source/Fonze/Private/CameraLooAt.cpp
+++ Source/Fonze/Private/CameraLooAt.cpp
@@ -1,5 +1,4 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Fonze.h"
 #include "CameraLooAt.h"
 
@@ -12,24 +11,39 @@ ACameraLooAt::ACameraLooAt()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	//Create our components
+	// Create our components
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
-	OurCameraSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraSpringArm"));
-	OurCameraSpringArm->AttachTo(RootComponent);
-	OurCameraSpringArm->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, 50.0f), FRotator(-80.0f, 0.0f, 0.0f));
-	OurCameraSpringArm->TargetArmLength = 400.f;
-	OurCameraSpringArm->bEnableCameraLag = true;
-	OurCameraSpringArm->CameraLagSpeed = 3.0f;
+	m_cameraSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraSpringArm"));
+	m_cameraSpringArm->AttachTo(RootComponent);
+	m_cameraSpringArm->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, 50.0f), FRotator(-80.0f, 0.0f, 0.0f));
+	m_cameraSpringArm->TargetArmLength = 400.f;
+	m_cameraSpringArm->bEnableCameraLag = true;
+	m_cameraSpringArm->CameraLagSpeed = 3.0f;
 
-	OurCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("GameCamera"));
-	OurCamera->AttachTo(OurCameraSpringArm, USpringArmComponent::SocketName);
+	// Attach camera spring arm to camera
+	m_camera = CreateDefaultSubobject<UCameraComponent>(TEXT("GameCamera"));
+	m_camera->AttachTo(m_cameraSpringArm, USpringArmComponent::SocketName);
+
+	// Init attributes
+	m_distanceMin = 100.0;
+	m_distanceMax = 1000.0;
+	m_targetDistance = 100.0;
+	m_currentDistance = m_targetDistance;
+	m_distanceIncrSpeed = 1.0;
+
+	m_aspectRatio = m_camera->AspectRatio;
+	m_cosFieldOfView = FMath::Abs(FMath::Cos(m_camera->FieldOfView));
+
+	for (TActorIterator<AHero> ActorItr(GetWorld()); ActorItr; ++ActorItr)
+	{
+		//m_heroesPositions.push_back(ActorItr->GetActorLocation());
+	}
 }
 
 // Called when the game starts or when spawned
 void ACameraLooAt::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
@@ -37,32 +51,84 @@ void ACameraLooAt::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
 
+	int i = 0;
+	for (TActorIterator<AHero> ActorItr(GetWorld()); ActorItr; ++ActorItr)
+	{
+		//m_heroesPositions.at(i) = ActorItr->GetActorLocation();
+		++i;
+	}
+
 	updatePosition();
+	updateDistanceTarget();
+	updateDistance();
 }
 
 void ACameraLooAt::updatePosition()
 {
 	FVector averageLocation = FVector(0.f, 0.f, 0.f);
 	uint8 numberOfActors = 0;
-
-	for (TActorIterator<AHero> ActorItr(GetWorld()); ActorItr; ++ActorItr)
+	/*
+	for (int i = 0; i < m_heroesPositions.size(); ++i)
 	{
-		averageLocation += ActorItr->GetActorLocation();
-		++numberOfActors;
+		averageLocation += m_heroesPositions.at(i);
 	}
 
-	if (numberOfActors)
+	if (m_heroesPositions.size())
 	{
-		averageLocation /= numberOfActors;
+		averageLocation /= m_heroesPositions.size();
 		SetActorLocation(averageLocation);
 	}
-		
+	*/
 }
+
+void ACameraLooAt::updateDistanceTarget()
+{
+	m_targetDistance = m_cosFieldOfView * getMaxHeroesDistance();
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("CosA = %f"), m_cosFieldOfView));
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("MaxDist = %f"), getMaxHeroesDistance()));
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("CamDist = %f"), m_targetDistance));
+	}
+
+	FMath::Clamp(m_targetDistance, m_distanceMin, m_distanceMax);
+}
+
+float ACameraLooAt::getMaxHeroesDistance()
+{
+	float maxDistance = 0.f;
+	/*
+	for (int i = 0; i < m_heroesPositions.size(); ++i)
+	{
+		for (int j = i + 1; j < m_heroesPositions.size(); ++j)
+		{
+			maxDistance = FMath::Max(maxDistance, FVector::Dist(m_heroesPositions.at(i), m_heroesPositions.at(j)));
+		}
+	}
+	*/
+
+	return maxDistance;
+}
+
+void ACameraLooAt::updateDistance()
+{
+	if (m_targetDistance > m_currentDistance + m_distanceIncrSpeed)
+	{
+		m_currentDistance += m_distanceIncrSpeed;
+		m_cameraSpringArm->TargetArmLength = m_currentDistance;
+	}
+	else if (m_targetDistance < m_currentDistance - m_distanceIncrSpeed)
+	{
+		m_currentDistance -= m_distanceIncrSpeed;
+		m_cameraSpringArm->TargetArmLength = m_currentDistance;
+	}
+}
+
 
 // Called to bind functionality to input
 void ACameraLooAt::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 {
 	Super::SetupPlayerInputComponent(InputComponent);
-
 }
 
--- Source/Fonze/Public/CameraLooAt.h
+++ Source/Fonze/Public/CameraLooAt.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "GameFramework/Pawn.h"
+// #include <vector>
 #include "CameraLooAt.generated.h"
 
 UCLASS()
@@ -10,24 +11,39 @@ class FONZE_API ACameraLooAt : public APawn
 {
 	GENERATED_BODY()
 
-public:
-	// Sets default values for this pawn's properties
-	ACameraLooAt();
+	public:
+		// Sets default values for this pawn's properties
+		ACameraLooAt();
 
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+		// Called when the game starts or when spawned
+		virtual void BeginPlay() override;
 	
-	// Called every frame
-	virtual void Tick( float DeltaSeconds ) override;
-	void updatePosition();
+		// Called every frame
+		virtual void Tick( float DeltaSeconds ) override;
+		void updatePosition();
+		void updateDistanceTarget();
+		void updateDistance();
+		float getMaxHeroesDistance();
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+		// Called to bind functionality to input
+		virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
-protected:
-	UPROPERTY(EditAnywhere)
-	USpringArmComponent* OurCameraSpringArm;
-	UCameraComponent* OurCamera;
+	protected:
+		UPROPERTY(EditAnywhere)
+		USpringArmComponent* m_cameraSpringArm;
+		UCameraComponent* m_camera;
+
+		float m_distanceMax;
+		float m_distanceMin;
+		float m_currentDistance;
+		float m_targetDistance;
+		float m_distanceIncrSpeed;
+
+		float m_aspectRatio;
+		float m_cosFieldOfView;
+
+		// std::vector<FVector> m_heroesPositions;
+		
 	
 	
 };
